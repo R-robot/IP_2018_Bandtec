da80ab7e9c4b636043ae28586a85ebd6b7b3231d
R-robot <Felipecf3@hotmail.com>
2018-09-06 19:02:59 -0300

    criação_modelagem_banco1

---
+++ Banco_de_dados/modelagem_1_receitas.sql
@@ -0,0 +1,17 @@
+create database banco_receitas;
+
+create table Receita (
+	id int primary key,
+    nome varchar(50),
+    tipo varchar(30)
+    );
+    
+    
+    select * from receita;
+    
+    desc receita;
+    
+    alter table receita auto_increment = 100;
+    
+    
+    
